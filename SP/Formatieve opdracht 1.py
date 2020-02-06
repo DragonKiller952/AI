41b0a27b31400739d0200d42f95ba582075f6192
DragonKiller952 <qjdegroot@gamil.com>
2020-02-06 13:35:33 +0100

    opgave 2

--- SP/Formatieve opdracht 1.py
+++ SP/Formatieve opdracht 1.py
@@ -1,4 +1,4 @@
-# Opg1
+# Opg1:
 
 lengte = int(input('hoe groot? '))
 lenwhile = 1
@@ -18,3 +18,33 @@ lenwhile -= 2
 while lenwhile > 0:
     print('*' * lenwhile)
     lenwhile -= 1
+
+# Opg2:
+
+zin1 = input('Geef een string: ')
+zin2 = input('Geef een string: ')
+
+if zin1 == zin2:
+    print('De strings zijn hetzelfde')
+else:
+    if len(zin1) != len(zin2):
+        if len(zin1) > len(zin2):
+            controlezin = zin1
+            fixzin = zin2 + ('*' * (len(zin1) - len(zin2)))
+        else:
+            controlezin = zin2
+            fixzin = zin1 + ('*' * (len(zin2) - len(zin1)))
+
+        for i in range(len(controlezin)):
+            if controlezin[i] != fixzin[i]:
+                verschil = i
+                break
+        print('Het eerste verschil zit op index: {}'.format(verschil))
+    else:
+        for i in range(len(zin1)):
+            if zin1[i] != zin2[i]:
+                verschil = i
+                break
+        print('Het eerste verschil zit op index: {}'.format(verschil))
+
+
