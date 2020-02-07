6a27cb18b1959bcd9ce069363c8ed197334ec988
DragonKiller952 <qjdegroot@gamil.com>
2020-02-07 17:47:30 +0100

    opgave 12

--- SP/Formatieve opdracht 1.py
+++ SP/Formatieve opdracht 1.py
@@ -276,3 +276,14 @@ else:
 
 print(''.join(cipher))
 
+# Opg12:
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
